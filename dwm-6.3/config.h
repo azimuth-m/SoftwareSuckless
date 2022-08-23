911bc04e7e862ed844fc465ecfe23e05a66ecb2d
azimuth-m <ifandakov@gmail.com>
2022-08-24 02:51:06 +0300

    asd

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -46,7 +46,7 @@ static const Rule rules[] = {
 	{ "Galculator",		NULL,       NULL,			0,			1,          0,			0,		    -1 },
 	{ "Pavucontrol",	NULL,       NULL,			0,			1,          0,			0,		    -1 },
 
-    { "st-256color",	NULL,		NULL,			0,			0,          1,			0,			-1 },
+    { "st",	NULL,		NULL,			0,			0,          1,			0,			-1 },
     { NULL,				NULL,		"Event Tester", 0,			0,          0,			1,			-1 }, /* xev */
 
 };
